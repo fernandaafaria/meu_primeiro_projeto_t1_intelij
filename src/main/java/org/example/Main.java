dd2eb09972f92b754f340a7f7916558420bc1c2b
FERNANDA FARIA DA SILVA <fernanda.silva124@senaisp.edu.br>
2026-08-28 14:34:08 -0300

    feat: Add my first java project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int valorInt = 12;
+        double valorDouble = valorInt; // casting implícito
+        System.out.println("Valor double: " + valorDouble);
+
+
+        }
+    }
+
