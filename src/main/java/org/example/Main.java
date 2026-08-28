71f2443258c36115df8f0fa21f97df0ea4d37332
fernadaafaria <fernandaafsilva007@gmail.com>
2026-08-28 16:03:30 -0300

    feat: Add nivel 1 - exercicio 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,16 +3,14 @@ package org.example;
 /*
 Nível 1: Casting implícito e explícito
 
-1. Casting Implícito: Declare uma variável do tipo int e atribua a ela um valor. Em seguida, crie uma variável do tipo double e atribua a ela o valor da variável int. Exiba o valor da variável double no console.
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+2. Casting Explícito: Declare uma variável do tipo double com um valor fracionado. Realize o casting explícito para uma variável do tipo int e exiba ambos os valores no console.
  */
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int valorInt = 12;
-        double valorDouble = valorInt; // casting implícito
-        System.out.println("Valor double: " + valorDouble);
+        double valorDouble = 33.7;
+        int valorInt = (int) valorDouble; //casting explícito
+        System.out.println("Valor Double: " + valorDouble);
+        System.out.println("Valor inteiro: " + valorInt);
 
 
         }
