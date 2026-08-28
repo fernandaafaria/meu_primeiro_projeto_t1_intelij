2c63b4e3c7d8a7d371fb960f48c7fadfbd054c2b
fernadaafaria <fernandaafsilva007@gmail.com>
2026-08-28 15:56:30 -0300

    feat: Add nivel 1 - exercicio 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,12 @@
 package org.example;
 
+/*
+Nível 1: Casting implícito e explícito
+
+1. Casting Implícito: Declare uma variável do tipo int e atribua a ela um valor. Em seguida, crie uma variável do tipo double e atribua a ela o valor da variável int. Exiba o valor da variável double no console.
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+ */
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
