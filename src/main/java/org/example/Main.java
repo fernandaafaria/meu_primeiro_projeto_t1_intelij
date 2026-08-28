dd866bd21390532615b5f40930638a7c7a038c29
fernadaafaria <fernandaafsilva007@gmail.com>
2026-08-28 16:33:13 -0300

    feat: Add nivel 2 - exercicio 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,23 @@
 package org.example;
 
 /*
-Nível 1: Casting implícito e explícito
+Nível 2: Operadores lógicos e relacionais
 
-4. Diferença entre Tipos de Dados: Declare uma variável do tipo short e atribua a ela um valor pequeno. Declare uma variável do tipo int e atribua a ela o valor da variável short (casting implícito). Depois, declare uma variável do tipo byte e atribua a ela o valor da variável short usando casting explícito. Exiba todos os valores no console.
+1. Comparação de Dois Números: Declare duas variáveis inteiras com valores diferentes. Compare-as utilizando operadores de comparação (>, <, >=, <=, ==, !=) e exiba os resultados das comparações no console.
  */
 public class Main {
     public static void main(String[] args) {
-        short valorShort = 32;
-        int valorInt = valorShort;
-        byte valorByte = (byte) valorShort;
+        int a,b;
+        a = 20;
+        b = 5;
+        boolean maior = a > b;
 
-        System.out.println("Valor short: " + valorShort);
-        System.out.println("Valor int: " + valorInt);
-        System.out.println("Valor byte: " + valorByte);
+        System.out.println("a maior que b? R: " +            maior);
+        System.out.println("a menor que b? R: " +           (a < b));
+        System.out.println("a igual a b? R: " +             (a == b));
+        System.out.println("a diferente de b? R: " +        (a != b));
+        System.out.println("a maior ou igual a b? R: " +    (a >= b));
+        System.out.println("a menor ou igual a b? R: " +    (a <= b));
 
 
 
