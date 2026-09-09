d2393c0af5165b80ba841ec4cd9b233d9b86f4e1
fernandaafaria <fernandaafsilva007@gmail.com>
2026-09-09 16:18:06 -0300

    feat: Add nivel 3 - exercicio 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,13 @@
 package org.example;
 import java.util.Scanner;
 //Nível 3: Processamento e Fórmulas Matemáticas
-//Exercicio 1: Média Aritmética: Receba três notas de um aluno (double) e calcule a média aritmética simples.
+//Exercicio 2: Conversor de Temperatura
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Digite a primeira nota: (10 a 0)");
-        double nota1= sc.nextDouble ();
-        System.out.println("Digite a segunda nota: (10 a 0)");
-        double nota2= sc.nextDouble ();
-        System.out.println("Digite a terceira nota: (10 a 0)");
-        double nota3= sc.nextDouble ();
-        double media = (nota1 + nota2 + nota3)/ 3;
-        System.out.println ("média:\n" + media);
+        System.out.println("Digite a temperatura em °C");
+        double celsius= sc.nextDouble ();
+        double faren = (celsius*9/5) + 32;
+        System.out.println ("A temperatura em farenheit:\n" + faren + " °F");
     }
 }
