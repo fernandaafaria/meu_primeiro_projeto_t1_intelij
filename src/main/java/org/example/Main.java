da3a29621579a32ea158ebb4a443d5db761198e9
fernandaafaria <fernandaafsilva007@gmail.com>
2026-09-10 09:52:20 -0300

    feat: Add nivel 4 - exercicio 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,15 @@
 package org.example;
 import java.util.Scanner;
 //Nível 4: Contexto Real e Conversões
-//Exercicio 2 - Consumo de Combustível: Peça a distância percorrida (km) e o total de combustível gasto (litros). Calcule o consumo médio (km/l).
+//Exercicio 3 - Salário Mensal: Solicite o valor da hora trabalhada e o número de horas trabalhadas no mês. Calcule o salário bruto.
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Digite a distância percorrida em km:");
-        double dist = entrada.nextDouble();
-        System.out.println("Digite o total de combustível gasto em litros:");
-        double litros = entrada.nextDouble();
-        double consumo = dist / litros;
-        System.out.println("Consumo médio:\n" + consumo + " km/l");
+        System.out.println("Digite o valor da hora trabalhada:");
+        double valorHora = entrada.nextDouble();
+        System.out.println("Digite o número de horas trabalhadas no mês:");
+        double horas = entrada.nextDouble();
+        double salario = valorHora * horas;
+        System.out.println("Salário bruto mensal:\n" + "R$ " + salario);
     }
 }
