0588b337cc3bc71554a154dc6100759d70d65400
fernandaafaria <fernandaafsilva007@gmail.com>
2026-09-10 10:11:35 -0300

    feat: Add nivel 5 - exercicio 5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,18 @@
 package org.example;
 import java.util.Scanner;
 //Nível 5: Desafios Integradores
-//Exercicio 4 -Cálculo de Juros Simples: Peça o capital inicial, a taxa de juros mensal e o tempo (meses).Calcule o montante final usando juros simples (J = C * i * t).
+//Exercicio 5 -Peça ao usuário uma quantidade total de segundos. Converta e exiba no formato: "X horas, Y minutos e Z segundos"
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Digite o capital inicial:");
-        double cap = entrada.nextDouble();
-        System.out.println("Digite a taxa de juros mensal (em %):");
-        double taxa = entrada.nextDouble();
-        System.out.println("Digite o tempo em meses:");
-        double tempo = entrada.nextDouble();
 
-        double juros = cap * (taxa / 100) * tempo;
-        double montante = cap + juros;
+        System.out.println("Digite a quantidade total de segundos:");
+        int totalSegundos = entrada.nextInt();
+        int horas = totalSegundos / 3600;
+        int resto = totalSegundos % 3600;
+        int minutos = resto / 60;
+        int segundos = resto % 60;
 
-        System.out.println("Montante final:\n" + "R$ " + montante);
+        System.out.println(horas + " horas, " + minutos + " minutos e " + segundos + " segundos");
     }
 }
