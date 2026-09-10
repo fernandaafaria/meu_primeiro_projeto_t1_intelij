a850f8bc39828208fa43d668f2e02b58b3d1e15b
fernandaafaria <fernandaafsilva007@gmail.com>
2026-09-10 09:58:26 -0300

    feat: Add nivel 5 - exercicio 4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,20 @@
 package org.example;
 import java.util.Scanner;
 //Nível 5: Desafios Integradores
-//Exercicio 3 -Divisão e Resto: Leia dois números inteiros. Exiba o quociente da divisão inteira e o resto da divisão (operador %).
+//Exercicio 4 -Cálculo de Juros Simples: Peça o capital inicial, a taxa de juros mensal e o tempo (meses).Calcule o montante final usando juros simples (J = C * i * t).
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Digite o primeiro número inteiro:");
-        int num1 = entrada.nextInt();
-        System.out.println("Digite o segundo número inteiro:");
-        int num2 = entrada.nextInt();
-        int quociente = num1 / num2;
-        int resto = num1 % num2;
-        System.out.println("Quociente da divisão:\n" + quociente);
-        System.out.println("Resto da divisão:\n" + resto);
+        System.out.println("Digite o capital inicial:");
+        double cap = entrada.nextDouble();
+        System.out.println("Digite a taxa de juros mensal (em %):");
+        double taxa = entrada.nextDouble();
+        System.out.println("Digite o tempo em meses:");
+        double tempo = entrada.nextDouble();
+
+        double juros = cap * (taxa / 100) * tempo;
+        double montante = cap + juros;
+
+        System.out.println("Montante final:\n" + "R$ " + montante);
     }
 }
