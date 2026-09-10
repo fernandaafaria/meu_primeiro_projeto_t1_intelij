d7e6b34c9e8d8c9d61da6599e4c6b395c33328dc
fernandaafaria <fernandaafsilva007@gmail.com>
2026-09-10 14:34:32 -0300

    feat: Add exercicios decisao - exercicio 9

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,17 +2,16 @@ package org.example;
 import java.util.Scanner;
 
 // Exercicios de estrutura de decisão
-// Exercício 8 - Auxílio Combustível
+// Exercício 9 - Auxilio para Curso de idiomas
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        System.out.print("Você possui veículo próprio? (sim/nao): ");
-        String resposta  = entrada.nextLine();
-
-        if (resposta.equalsIgnoreCase("Sim")) {
-            System.out.println("Auxílio combustível disponível.");
+        System.out.println("Digite o cargo do funcionário:");
+        String cargo = entrada.nextLine();
+        if (cargo.equalsIgnoreCase("administrativo") || cargo.equalsIgnoreCase("liderança") || cargo.equalsIgnoreCase("lideranca")) {
+            System.out.println("Você pode participar do programa de idiomas.");
         } else {
-            System.out.println("Não possui auxílio combustível.");
+            System.out.println("Você não tem acesso ao programa de idiomas no momento.");
         }
     }
 }
