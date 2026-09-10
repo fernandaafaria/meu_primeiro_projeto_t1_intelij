1e76d97996e0ae8801e6a258e35e48fa9b2489d4
fernandaafaria <fernandaafsilva007@gmail.com>
2026-09-10 09:48:08 -0300

    feat: Add nivel 3 - exercicio 5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,15 @@
 package org.example;
 import java.util.Scanner;
 //Nível 3: Processamento e Fórmulas Matemáticas
-//Exercicio 4 - Perímetro do Retângulo: Solicite a base e a altura de um retângulo. Calcule e exiba o perímetro.
+//Exercicio 5 - Cálculo de Desconto: Leia o valor de um produto e aplique um desconto fixo de 15%. Exiba o novo valor.
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Digite o valor da base em metros:");
-        double base = entrada.nextDouble();
-        System.out.println("Digite o valor da altura em metros:");
-        double altura = entrada.nextDouble();
-        double peri = base + altura + base + altura;
-        System.out.println("Perímetro do retângulo:\n" + peri + " m");
+        System.out.println("Digite o valor do produto:");
+        double prod = entrada.nextDouble();
+        System.out.println("Digite a porcentagem de desconto:");
+        double desc = 0.15;
+        double valorReal = prod - (prod * desc);
+        System.out.println("Preço do produto com desconto:\n" + "R$" + valorReal);
     }
 }
