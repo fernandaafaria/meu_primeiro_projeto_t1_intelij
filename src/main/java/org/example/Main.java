6591390d699b649f6989b25d54a2765268a467b5
fernandaafaria <fernandaafsilva007@gmail.com>
2026-09-10 14:05:46 -0300

    feat: Add exercicios decisao - exercicio 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,18 +2,20 @@ package org.example;
 import java.util.Scanner;
 
 // Exercicios de estrutura de decisão
-// Questão 2 -Aprovação do aluno
+// Questão 3 - Comparação de notas
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        double nota;
-        System.out.println("Digite sua nota:");
-        nota = entrada.nextDouble();
+        double n1, n2;
+        System.out.println("Digite o primeiro número: ");
+        n1 = entrada.nextDouble();
+        System.out.println("Digite o segundo número: ");
+        n2 = entrada.nextDouble();
 
-        if (nota >=7 ){
-            System.out.println("Aprovado!");
+        if (n1 > n2 ){
+            System.out.println(n1);
         } else {
-            System.out.println("Reprovado!");
+            System.out.println(n2);
         }
     }
 }
