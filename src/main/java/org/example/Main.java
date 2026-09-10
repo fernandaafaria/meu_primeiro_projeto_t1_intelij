40d9fa95f1d6cbf0dfc6e3fe04471b8da388a87f
fernandaafaria <fernandaafsilva007@gmail.com>
2026-09-10 09:57:38 -0300

    feat: Add nivel 5 - exercicio 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,17 @@
 package org.example;
 import java.util.Scanner;
 //Nível 5: Desafios Integradores
-//Exercicio 2 -Conversor de Medidas: Leia um valor em metros e exiba-o convertido para centímetros e milímetros.
+//Exercicio 3 -Divisão e Resto: Leia dois números inteiros. Exiba o quociente da divisão inteira e o resto da divisão (operador %).
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Digite o valor em metros:");
-        double metros = entrada.nextDouble();
-        double cm = metros * 100;
-        double mm = metros * 1000;
-        System.out.println("Valor convertido:\n" + cm + " cm\n" + mm + " mm");
+        System.out.println("Digite o primeiro número inteiro:");
+        int num1 = entrada.nextInt();
+        System.out.println("Digite o segundo número inteiro:");
+        int num2 = entrada.nextInt();
+        int quociente = num1 / num2;
+        int resto = num1 % num2;
+        System.out.println("Quociente da divisão:\n" + quociente);
+        System.out.println("Resto da divisão:\n" + resto);
     }
 }
