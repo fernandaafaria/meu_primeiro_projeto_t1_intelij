f692e907724761c71200018504e098f2d3552742
fernandaafaria <fernandaafsilva007@gmail.com>
2026-09-10 09:41:41 -0300

    feat: Add nivel 3 - exercicio 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,13 @@
 package org.example;
 import java.util.Scanner;
 //Nível 3: Processamento e Fórmulas Matemáticas
-//Exercicio 2: Conversor de Temperatura
+//Exercicio 3 - Área do Quadrado: Peça o valor do lado de um quadrado e calcule sua área (A = lado * lado).
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Digite a temperatura em °C");
-        double celsius= sc.nextDouble ();
-        double faren = (celsius*9/5) + 32;
-        System.out.println ("A temperatura em farenheit:\n" + faren + " °F");
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite o valor do lado em metros:");
+        double base = entrada.nextDouble();
+        double area = base * base;
+        System.out.println("Área do quadrado:\n" + area + " m²");
     }
 }
