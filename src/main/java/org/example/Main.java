329e872980fcde762af0f528c6e9946bb9db3f32
fernandaafaria <fernandaafsilva007@gmail.com>
2026-09-10 09:56:50 -0300

    feat: Add nivel 5 - exercicio 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,14 @@
 package org.example;
 import java.util.Scanner;
 //Nível 5: Desafios Integradores
-//Exercicio 1 -Cálculo de IMC: Peça o peso e a altura de uma pessoa. Calcule o Índice de Massa Corporal (IMC = peso / altura²).
+//Exercicio 2 -Conversor de Medidas: Leia um valor em metros e exiba-o convertido para centímetros e milímetros.
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Digite o peso em kg:");
-        double peso = entrada.nextDouble();
-        System.out.println("Digite a altura em metros:");
-        double altura = entrada.nextDouble();
-        double imc = peso / (altura * altura);
-        System.out.println("Índice de Massa Corporal (IMC):\n" + imc);
+        System.out.println("Digite o valor em metros:");
+        double metros = entrada.nextDouble();
+        double cm = metros * 100;
+        double mm = metros * 1000;
+        System.out.println("Valor convertido:\n" + cm + " cm\n" + mm + " mm");
     }
 }
