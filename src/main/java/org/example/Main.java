543158334d6582d0bfb086cda83e3bd33b1393eb
fernandaafaria <fernandaafsilva007@gmail.com>
2026-09-10 09:45:21 -0300

    feat: Add nivel 3 - exercicio 4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,15 @@
 package org.example;
 import java.util.Scanner;
 //Nível 3: Processamento e Fórmulas Matemáticas
-//Exercicio 3 - Área do Quadrado: Peça o valor do lado de um quadrado e calcule sua área (A = lado * lado).
+//Exercicio 4 - Perímetro do Retângulo: Solicite a base e a altura de um retângulo. Calcule e exiba o perímetro.
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Digite o valor do lado em metros:");
+        System.out.println("Digite o valor da base em metros:");
         double base = entrada.nextDouble();
-        double area = base * base;
-        System.out.println("Área do quadrado:\n" + area + " m²");
+        System.out.println("Digite o valor da altura em metros:");
+        double altura = entrada.nextDouble();
+        double peri = base + altura + base + altura;
+        System.out.println("Perímetro do retângulo:\n" + peri + " m");
     }
 }
