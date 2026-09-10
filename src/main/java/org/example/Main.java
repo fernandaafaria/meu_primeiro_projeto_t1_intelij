8030c8a7135c70e1fa98079c7201aef6a69fce95
fernandaafaria <fernandaafsilva007@gmail.com>
2026-09-10 14:36:54 -0300

    feat: Add exercicios decisao - exercicio 10

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,16 +2,23 @@ package org.example;
 import java.util.Scanner;
 
 // Exercicios de estrutura de decisão
-// Exercício 9 - Auxilio para Curso de idiomas
+// Exercício 10 - Aprovação de Empréstimo Pessoal
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Digite o cargo do funcionário:");
-        String cargo = entrada.nextLine();
-        if (cargo.equalsIgnoreCase("administrativo") || cargo.equalsIgnoreCase("liderança") || cargo.equalsIgnoreCase("lideranca")) {
-            System.out.println("Você pode participar do programa de idiomas.");
+
+        System.out.println("Digite o salário do cliente:");
+        double salario = entrada.nextDouble();
+
+        System.out.println("Digite o valor da parcela desejada:");
+        double parcela = entrada.nextDouble();
+
+        double limite = salario * 0.30;
+
+        if (parcela <= limite) {
+            System.out.println("Empréstimo aprovado.");
         } else {
-            System.out.println("Você não tem acesso ao programa de idiomas no momento.");
+            System.out.println("Empréstimo recusado. A parcela excede 30% do salário.");
         }
     }
 }
