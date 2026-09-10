01ee42687410b322593ccaf77c2784f85856e29a
fernandaafaria <fernandaafsilva007@gmail.com>
2026-09-10 16:35:44 -0300

    feat: Add exercicios de repeticao - exercicio 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,20 @@
-package org.example;
-import java.util.Scanner;
+//Estrtura de repetição
+ // Exercicio  1 - Controle de Entrada em Evento
 
-// Exercicios de estrutura de decisão
-// Exercício 14 - Concessão de Isenção de Tarifas
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Digite o saldo médio mensal do cliente (R$):");
-        double saldo = entrada.nextDouble();
 
-        if (saldo > 5000) {
-            System.out.println("Direito à isenção de tarifa bancária concedido.");
-        } else {
-            System.out.println("Tarifa bancária mantida. É necessário saldo superior a R$ 5.000 para isenção.");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Digite o nome do participante " + i + ":");
+            String nome = entrada.nextLine();
+
+            System.out.println("Entrada registrada com sucesso: " + nome + " (Participante " + i + "/10)");
         }
+
+        System.out.println("Todos os 10 participantes foram registrados!");
+        entrada.close();
     }
 }
