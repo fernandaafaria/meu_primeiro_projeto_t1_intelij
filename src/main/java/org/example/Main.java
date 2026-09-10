981f5daf59b23b4fe7f3503d5a6be5e7b24d472f
fernandaafaria <fernandaafsilva007@gmail.com>
2026-09-10 09:55:59 -0300

    feat: Add nivel 5 - exercicio 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,15 @@
 package org.example;
 import java.util.Scanner;
-//Nível 4: Contexto Real e Conversões
-//Exercicio 5 -Troca de Valores: Leia dois valores A e B. Troque os valores das variáveis de forma que A passe a ter o valor de B e vice-versa. Exiba os valores antes e depois da troca.
+//Nível 5: Desafios Integradores
+//Exercicio 1 -Cálculo de IMC: Peça o peso e a altura de uma pessoa. Calcule o Índice de Massa Corporal (IMC = peso / altura²).
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Digite o valor de A:");
-        int a = entrada.nextInt();
-        System.out.println("Digite o valor de B:");
-        int b = entrada.nextInt();
-
-        System.out.println("Valores antes da troca:\nA: " + a + "\nB: " + b);
-
-        int aux = a;
-        a = b;
-        b = aux;
-
-        System.out.println("Valores depois da troca:\nA: " + a + "\nB: " + b);
+        System.out.println("Digite o peso em kg:");
+        double peso = entrada.nextDouble();
+        System.out.println("Digite a altura em metros:");
+        double altura = entrada.nextDouble();
+        double imc = peso / (altura * altura);
+        System.out.println("Índice de Massa Corporal (IMC):\n" + imc);
     }
 }
