8016a30674d9108d9430643b4ccce8c529accf28
fernandaafaria <fernandaafsilva007@gmail.com>
2026-09-11 14:22:22 -0300

    feat: Add exercicios de repeticao - exercicio 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,10 @@
 //Estrtura de repetição
- // Exercicio  1 - Controle de Entrada em Evento
-
-import java.util.Scanner;
+// Exercício 2 - Controle de Produção Industrial
 
 public class Main {
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-
-        for (int i = 1; i <= 10; i++) {
-            System.out.println("Digite o nome do participante " + i + ":");
-            String nome = entrada.nextLine();
-
-            System.out.println("Entrada registrada com sucesso: " + nome + " (Participante " + i + "/10)");
+        for (int peca = 1; peca <= 20; peca++) {
+            System.out.println("Peça Produzida");
         }
-
-        System.out.println("Todos os 10 participantes foram registrados!");
-        entrada.close();
     }
 }
